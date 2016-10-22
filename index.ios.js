06a2395505572871c1230ee047e51646fe761861
Andrew Heekin <andrewheekin@gmail.com>
2016-10-22 12:15:27 -0400

    init

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+
+AppRegistry.registerComponent('albums', () => App);
