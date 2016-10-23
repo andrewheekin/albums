dfadfb004d1d9dc15a6db46d788498b0262ba3ff
Andrew Heekin <andrewheekin@gmail.com>
2016-10-23 19:43:02 -0400

    card, cardsection, albumdetail

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
-    <View style ={viewStyle}>
+    <View style={viewStyle}>
       <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
