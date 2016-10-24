8a11a87697a6f606a9a2f6544666db63a0c8e90e
Andrew Heekin <andrewheekin@gmail.com>
2016-10-24 19:40:46 -0400

    link to album

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -7,7 +7,7 @@ import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   // destructure styles object and props
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
 
   return (
@@ -25,9 +25,9 @@ const AlbumDetail = ({ album }) => {
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
-      
+
       <CardSection>
-        <Button />
+        <Button onPress={() => Linking.openURL(url) }>Buy Now</Button>
       </CardSection>      
     </Card>
   );
